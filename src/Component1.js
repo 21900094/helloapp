ace3e46b57e7946c03a2b422c2a901e16672774c
21900094 <102216616+21900094@users.noreply.github.com>
2024-11-08 22:34:50 +0900

    changed

---
+++ src/Component1.js
@@ -0,0 +1,15 @@
+
+
+function Component1() {
+    return (
+      <div>
+        <h2>About Me</h2>
+        <p>
+          한동대학교 전산전자공학부 컴퓨터공학과
+        </p>
+      </div>
+    );
+  }
+  
+  export default Component1;
+  
---
+++ src/Component2.js
@@ -0,0 +1,14 @@
+
+function Component2() {
+    return (
+      <div>
+        <h2>Hobbies & Interests</h2>
+        <p>
+          영화, 운동
+        </p>
+      </div>
+    );
+  }
+  
+  export default Component2;
+  
